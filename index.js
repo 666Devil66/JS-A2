ff5743e9f9526b9d5bc332967974c27b8a4541be
Devil <152491504+666Devil66@users.noreply.github.com>
2023-12-25 13:20:43 +0530

    JS A2

---
+++ index.js
@@ -0,0 +1,41 @@
+//Q1
+let a = 5;
+let b = 2;
+console.log(a + b);
+
+//Q2
+let a1 = 5;
+let b1 = 3;
+console.log(a1<10 && a1>b1);
+
+//Q3
+// let c=20;
+// let d=10;
+// console.log(c%10==0 && d%10==0) 
+// console.log(c%10!=0 && d%10!=0) 
+// console.log(c%10==0 || d%10==0) 
+
+//Q4
+// let num = "4567";
+// console.log(num[0]);
+
+//Q5
+// let  num1 = "4567";
+// console.log(num1[3]);
+
+//Q6
+// let a = 3;
+// let b = 16;
+// console.log( b % a);
+
+//Q7
+// let x = 2;
+// let y = 5;
+// console.log(x * y);
+
+//Q8
+//  let x = 50;
+//  let y = 20;
+//  let z = 100;
+//  let a = x+y+z
+//  console.log(a, a/3)
